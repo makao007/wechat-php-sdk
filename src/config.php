8ce06a988573ba3f433b07640b1e0159ba1e489f
makao007 <makao007@126.com>
2017-01-05 13:23:55 +0800

    x

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 
-  $encodingAesKey = "";
-  $token = "";
-  $appId = "";
+  $encodingAesKey = "691f1ff7a81e77d13b75dad07f6fb2bc";
+  $token = "helloxxxayasd";
+  $appId = "wx163fa2f27f02d686";
   
   $debugMode = TRUE;
 
